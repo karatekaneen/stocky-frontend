11c5ed500a3506501ccf091562484849e6c87107
karatekaneen <bauhn_@hotmail.com>
2020-08-02 16:06:28 +0200

    Project init

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		Login component
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+	// Stuff
+})
+</script>
+
+<style scoped></style>
---
+++ src/views/Login.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>Här ska det vara login</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+	// Stuff
+})
+</script>
+
+<style scoped></style>
